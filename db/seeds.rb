760266851df4b65a669af8c0825e94f3cad8b8f0
br0nc080 <mwelbu2@gmail.com>
2011-06-18 19:10:01 -0500

    minor update to remove requirements

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,6 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-require 'faker'
-require 'populator'
-
 Truck.destroy_all
 Location.destroy_all
 
@@ -40,3 +37,5 @@ Truck.all.each do |truck|
     location.save
   end
 end
+
+{"listed_count"=>0, "protected"=>false, "url"=>"http://www.flickr.com/photos/br0nc080/", "profile_text_color"=>"333333", "name"=>"Mike", "created_at"=>"Sun Mar 22 18:04:00 +0000 2009", "profile_sidebar_fill_color"=>"DDFFCC", "id_str"=>"25851576", "following"=>nil, "notifications"=>nil, "profile_background_tile"=>true, "utc_offset"=>-21600, "is_translator"=>false, "default_profile_image"=>false, "description"=>"", "contributors_enabled"=>false, "friends_count"=>70, "profile_link_color"=>"0084B4", "status"=>{"geo"=>nil, "created_at"=>"Thu Jun 16 04:00:38 +0000 2011", "id_str"=>"81209542549061633", "coordinates"=>nil, "text"=>"@bkish_delights oooooo", "favorited"=>false, "in_reply_to_status_id_str"=>"81207505484660737", "in_reply_to_screen_name"=>"bkish_delights", "in_reply_to_user_id_str"=>"26430313", "truncated"=>false, "id"=>81209542549061633, "contributors"=>nil, "retweeted"=>false, "source"=>"<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>", "retweet_count"=>0, "place"=>nil, "in_reply_to_user_id"=>26430313, "in_reply_to_status_id"=>81207505484660737}, "default_profile"=>false, "favourites_count"=>9, "profile_sidebar_border_color"=>"BDDCAD", "followers_count"=>42, "profile_image_url"=>"http://a2.twimg.com/profile_images/106539542/Kool-AidMan_normal.jpg", "show_all_inline_media"=>false, "follow_request_sent"=>nil, "geo_enabled"=>false, "time_zone"=>"Central Time (US & Canada)", "id"=>25851576, "verified"=>false, "profile_use_background_image"=>true, "screen_name"=>"br0nc080", "lang"=>"en", "profile_background_color"=>"1d2c3f", "location"=>"Illinois", "statuses_count"=>665, "profile_background_image_url"=>"http://a0.twimg.com/profile_background_images/34772678/arrelious_benn2.jpg"}
